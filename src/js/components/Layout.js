e7d7d26d8dcb59d9d6f33647c4fd92916caa8063
Luka Klacar <l.klacar@vegaitsourcing.rs>
2017-12-27 14:58:26 +0100

    Initial commit

---
+++ src/js/components/Layout.js
@@ -0,0 +1,39 @@
+import React from "react"
+import {connect} from "react-redux"
+
+import {fetchUser} from "../actions/userActions"
+import {fetchTweets} from "../actions/tweetsActions"
+import {bindActionCreators} from "redux";
+
+
+const mapStateToProps = (store) => {
+    return {
+        user: store.user.user,
+        userFetched: store.user.fetched,
+        tweets: store.tweets.tweets,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {actions: bindActionCreators({fetchUser, fetchTweets}, dispatch)}
+};
+
+@connect(mapStateToProps, mapDispatchToProps)
+export default class Layout extends React.Component {
+    componentWillMount() {
+        this.props.actions.fetchUser();
+    }
+
+    render() {
+        const {user, tweets} = this.props;
+
+        if (!tweets.length) {
+            return <button onClick={() => this.props.actions.fetchTweets()}>load tweets</button>
+        }
+
+        return <div>
+            <h1>{user.name}</h1>
+            <ul>{tweets.map(tweet => <li key={tweet.id}>{tweet.text}</li>)}</ul>
+        </div>
+    }
+}
