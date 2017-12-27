c82baae3f4bdc539cdf88a374362e691cf3cdb81
Luka Klacar <l.klacar@vegaitsourcing.rs>
2017-12-27 15:29:07 +0100

    Major refactoring

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
-
 import Layout from "./components/Layout"
 import store from "./store"
+import registerServiceWorker from './registerServiceWorker';
 
-const app = document.getElementById('app')
+const app = document.getElementById('root');
 
 ReactDOM.render(<Provider store={store}>
     <Layout/>
 </Provider>, app);
+
+registerServiceWorker();
