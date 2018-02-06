41d9a3ce584adff92e4f603107337c9d629c92d7
Luka Klacar <l.klacar@vegaitsourcing.rs>
2018-02-06 13:07:33 +0100

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -2,13 +2,17 @@ import React from "react"
 import ReactDOM from "react-dom"
 import {Provider} from "react-redux"
 import Layout from "./components/Layout"
-import store from "./store"
 import registerServiceWorker from './registerServiceWorker';
+import { PersistGate } from 'redux-persist/integration/react'
+import {persistor, store} from "./store";
 
 const app = document.getElementById('root');
 
+console.log(store);
 ReactDOM.render(<Provider store={store}>
-    <Layout/>
+    <PersistGate loading={null} persistor={persistor}>
+        <Layout/>
+    </PersistGate>
 </Provider>, app);
 
 registerServiceWorker();
--- src/store.js
+++ src/store.js
@@ -3,9 +3,24 @@ import {applyMiddleware, createStore} from "redux"
 import logger from "redux-logger"
 import thunk from "redux-thunk"
 import promise from "redux-promise-middleware"
-
 import reducer from "./reducers"
+import {persistStore, persistReducer} from 'redux-persist'
 
-const middleware = applyMiddleware(promise(), thunk, logger())
 
-export default createStore(reducer, middleware)
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web and AsyncStorage for react-native
+const middleware = applyMiddleware(promise(), thunk, logger());
+
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducer)
+
+// export default createStore(reducer, middleware)
+
+let store = createStore(persistedReducer, middleware);
+let persistor = persistStore(store);
+
+export {store, persistor}
